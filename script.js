933c2aec4c47dff19ee15bea45de272eb64aa060
lerapro <lera.protcyshina@gmail.com>
2023-08-24 12:40:57 +0300

    commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// --------TASK 1-----------
+
+// let num = 0;
+
+// while (num < 10) {
+//     num++;
+//     console.log(num);
+// }
+
+// --------TASK 2-----------
+
+// for (let i = 1; i <= 20; i++){
+//     if (i % 2 != 0) {
+//         continue
+//     }
+
+//     console.log(i);
+// }
+
+// --------TASK 3-----------
+
+// let number;
+
+// for (let i = 1; i <= 10; i++){
+//     number = 7 * i;
+//     console.log(number);
+// }
+
+// --------TASK 4-----------
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// let i = 0;
+
+// while (i < numbers.length) {
+//     console.log(numbers[i]);
+//     i++;
+// }
+
+// --------TASK 5-----------
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+
+//     if (arr[i] === 7) {
+//         break
+//     }
+// }
+
+// --------TASK 6-----------
+
+// let n = prompt("Enter any number");
+// let i = 0;
+
+// while (i < n) {
+//     console.log(i);
+//     i++;
+
+//     if (i >= n) {
+//         break;
+//     }
+// }
+
+// --------TASK 7-----------
+
+// let i = 1;
+
+// while (i <= 20) {
+//     console.log(i);
+//     i++;
+
+//     if (i % 3 == 0) {
+//         i++;
+//         continue;
+//     }
+//  }
