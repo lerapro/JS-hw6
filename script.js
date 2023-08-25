3f0dfb65796a4dbcc8b516e0ade79c00781c347f
lerapro <lera.protcyshina@gmail.com>
2023-08-25 18:38:27 +0300

    commit

--- script.js
+++ script.js
@@ -42,11 +42,12 @@
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // for (let i = 0; i < arr.length; i++){
-//     console.log(arr[i]);
 
 //     if (arr[i] === 7) {
 //         break
 //     }
+
+//     console.log(arr[i]);
 // }
 
 // --------TASK 6-----------
